b917d296ac1fb84ec654c6c4fae3393f02a480a0
Stefan Maimone <smaimo@profactor.at>
2023-03-30 12:32:01 +0200

    changed signatures and added option to add reverse edges to cycle

--- subgraph_counting/pattern.py
+++ subgraph_counting/pattern.py
@@ -4,12 +4,12 @@ This module counts subgraph structures in a graph with conventional algorithms.
 from igraph import Graph
 from itertools import combinations
 
-def create_cycle(vertex_count: int) -> Graph:
+def create_cycle(node_count: int, duplicate_edges = False) -> Graph:
     """ Creates an undirected cycle.
 
     Parameters
     ----------
-    vertex_count : int
+    node_count : int
         number of nodes in the cycle.
         cycle: all vertices are connected in a closed chain.
 
@@ -17,16 +17,18 @@ def create_cycle(vertex_count: int) -> Graph:
     ----------
     Undirected cycle.
     """
-    edges = list(zip(range(vertex_count-1), range(1,vertex_count)))
-    edges.append((vertex_count-1, 0))
-    return Graph(n=vertex_count, edges=edges)
+    edges = list(zip(range(node_count-1), range(1,node_count)))
+    edges.append((node_count-1, 0))
+    if duplicate_edges:
+        edges = edges + [(edge[1], edge[0]) for edge in edges]
+    return Graph(n=node_count, edges=edges)
 
-def create_path(vertex_count: int):
+def create_path(node_count: int):
     """ Creates an undirected path.
 
     Parameters
     ----------
-    vertex_count : int
+    node_count : int
         number of nodes in the path.
         path: all vertices are connected in an open chain
     
@@ -34,15 +36,15 @@ def create_path(vertex_count: int):
     ----------
     Undirected path.
     """
-    edges = list(zip(range(vertex_count-1), range(1,vertex_count)))
-    return Graph(n=vertex_count, edges=edges)
+    edges = list(zip(range(node_count-1), range(1,node_count)))
+    return Graph(n=node_count, edges=edges)
 
-def create_clique(vertex_count: int):
+def create_clique(node_count: int):
     """ Creates an undirected clique.
 
     Parameters
     ----------
-    vertex_count : int
+    node_count : int
         number of nodes in the clique structure
         clique: all pair of nodes are connected by an edge.
 
@@ -50,22 +52,22 @@ def create_clique(vertex_count: int):
     ----------
     Undirected clique.
     """
-    vertices = list(range(vertex_count))
+    vertices = list(range(node_count))
     edges = list(combinations(vertices, 2))
-    return Graph(n=vertex_count, edges=edges)
+    return Graph(n=node_count, edges=edges)
 
-def create_star(vertex_count: int):
+def create_star(node_count: int):
     """ Creates an undirected star.
 
     Parameters
     ----------
-    vertex_count : int
+    node_count : int
         number of nodes in the star structure.
-        star: a central vertex is connected to all other vertices.
+        star: a central node is connected to all other vertices.
 
     Returns
     ----------
     Undirected star.
     """
-    edges = [(0, x + 1) for x in range(vertex_count - 1)]
-    return Graph(n=vertex_count, edges=edges)
+    edges = [(0, x + 1) for x in range(node_count - 1)]
+    return Graph(n=node_count, edges=edges)
